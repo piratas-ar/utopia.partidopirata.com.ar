521249f6dbc1b56ecee5fd7c02ea588f62f5438b
fauno <fauno@endefensadelsl.org>
2019-06-11 18:58:04 -0300

    urls e indice relativos

--- _plugins/sample_filter.rb
+++ _plugins/sample_filter.rb
@@ -14,6 +14,24 @@ module Jekyll
       def month_i18n(date, months)
         months[date.month - 1]
       end
+
+      def base(url)
+        base = ''
+        depth = url.split('/').size - 2
+        depth.times do
+          base += '../'
+        end
+
+        base
+      end
+
+      # TODO: Mover a jekyll-relative-urls
+      # TODO: Hacerlo con Nokogiri?
+      #
+      # Agrega una URL base a los anchors de una cadena HTML
+      def html_relativize_anchors_to(input, base)
+        input.gsub!(/((href|src)=["'])#/, "\\1#{base}#")
+      end
     end
   end
 end
